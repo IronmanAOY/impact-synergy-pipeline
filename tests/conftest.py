8bd54337c521b1c643a693f8fe06c7d285947fa2
IronmanAOY <99022289+IronmanAOY@users.noreply.github.com>
2025-04-30 12:46:02 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,9 @@
-# -*- coding: utf-8 -*-# tests/conftest.pyimport sysfrom pathlib import Path# ensure project root is on pytest’s PYTHONPATHroot = Path(__file__).resolve().parent.parentif str(root) not in sys.path:    sys.path.insert(0, str(root))
+# -*- coding: utf-8 -*-
+# tests/conftest.py
+import sys
+from pathlib import Path
+
+# ensure project root is on pytest√ïs PYTHONPATH
+root = Path(__file__).resolve().parent.parent
+if str(root) not in sys.path:
+    sys.path.insert(0, str(root))
